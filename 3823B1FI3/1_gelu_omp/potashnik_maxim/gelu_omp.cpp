8fbd8b376519f31cacfa46f1c331535b50d368ee
Max <scourgetp@gmail.com>
2026-05-09 11:46:54 +0300

    [TASK1] Inlined func into loop

--- 3823B1FI3/1_gelu_omp/potashnik_maxim/gelu_omp.cpp
+++ 3823B1FI3/1_gelu_omp/potashnik_maxim/gelu_omp.cpp
@@ -5,20 +5,15 @@
 
 /* Optimizations
 
-1. Optimized formula
+1. Optimized calculations
 tanh(x) = (e^x - e^(-x)) / (e^x + e^(-x)) = 1 - 2 / (e^(2*x) + 1) 
+2. Inlined function into loop
 
 */
 
-float GELU(const float& x) {
-    constexpr float sqrt_2_over_pi = 0.7978845608028654f;
-    constexpr float coeff = 0.044715f;
-    float x3 = x * x * x;
-    float exp_arg = 2 * sqrt_2_over_pi * (x + coeff * x3);
-    float exp_val = expf(exp_arg);
-    float tanh_val = 1 - 2 / (exp_val + 1);
-    return 0.5 * x * (1.0f + tanh_val);
-} 
+constexpr float sqrt_2_over_pi = 0.7978845608028654f;
+constexpr float coeff = 0.044715f;
+
 
 std::vector<float> GeluOMP(const std::vector<float>& input)
 {
@@ -27,7 +22,14 @@ std::vector<float> GeluOMP(const std::vector<float>& input)
     
     for (int i = 0; i < sz; i++)
     {
-        res[i] = GELU(input[i]);
+        float x = input[i];
+
+        float x3 = x * x * x;
+        float exp_arg = 2 * sqrt_2_over_pi * (x + coeff * x3);
+        float exp_val = expf(exp_arg);
+        float tanh_val = 1 - 2 / (exp_val + 1);
+
+        res[i] = 0.5 * x * (1.0f + tanh_val);
     }
     return res;
 }
